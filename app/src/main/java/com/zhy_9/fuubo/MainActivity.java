8640a1a81db1da75be6867bbb12e38598f396dbd
zxk901021 <447360581@qq.com>
2015-05-13 16:52:53 +0800

    chen jin shi

--- app/src/main/java/com/zhy_9/fuubo/MainActivity.java
+++ app/src/main/java/com/zhy_9/fuubo/MainActivity.java
@@ -1,27 +1,57 @@
 package com.zhy_9.fuubo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+    private SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        if (toolbar != null){
+        if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
         if (android.os.Build.VERSION.SDK_INT >= 19) {
             getWindow().getDecorView().setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+
+        refreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh_layout);
+//        refreshLayout.setColorSchemeColors(android.R.color.holo_blue_dark, android.R.color.holo_blue_light,
+//                android.R.color.holo_green_light, android.R.color.holo_orange_light);//4.4上此方法不起作用
+        refreshLayout.setColorScheme(android.R.color.holo_blue_dark, android.R.color.holo_blue_light,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshLayout.setRefreshing(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        refreshLayout.setRefreshing(false);
+
+                    }
+                }, 2500);
+            }
+        });
+
+        LayoutInflater inflater = getLayoutInflater();
+        View toastLayout = inflater.inflate(R.layout.toast_layout, null);
+        TextView toastText = (TextView) toastLayout.findViewById(R.id.toast_content);
+        toastText.setText("没有新消息了");
+
     }
 
 
