70a072162bcf8dd9043538fcf1051d120c3e5030
zxk901021 <447360581@qq.com>
2015-05-14 17:43:27 +0800

    chen jin shi

--- app/src/main/java/com/zhy_9/fuubo/WelcomeActivity.java
+++ app/src/main/java/com/zhy_9/fuubo/WelcomeActivity.java
@@ -4,13 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
+
 
 public class WelcomeActivity extends Activity {
-    
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
