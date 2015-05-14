178037b6950c3de7cca5bf6b8fb62ed9807a1c6f
zxk901021 <447360581@qq.com>
2015-05-14 17:40:15 +0800

    Update WelcomeActivity.java

--- app/src/main/java/com/zhy_9/fuubo/WelcomeActivity.java
+++ app/src/main/java/com/zhy_9/fuubo/WelcomeActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.Button;
 
 public class WelcomeActivity extends Activity {
+    
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
